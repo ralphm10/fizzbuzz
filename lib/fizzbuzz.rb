2b29bde433ca594df4046dfa1be6a0648d1cc754
harrylb14 <harry.lingardbright@gmail.com>
2020-06-30 10:18:42 +0000

    added method for multiples of 5 and 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
-  if number % 3 != 0
-    if number % 5 == 0
-      'buzz'
-    else 
-    number
-    end 
-  else
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
     'fizz'
-  end
+  elsif number % 5 == 0
+    'buzz'
+  else 
+    number
+  end 
 end
